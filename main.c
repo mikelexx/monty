8aac8105591a68219116d68a81f34812e22740f6
MICHAEL MURITHI <michael.muri2020@gmail.com>
2024-01-17 12:16:41 +0300

    add check for blank lines in bytecode files, they are valid

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 	{
 		opcode = strtok(line_input, delim);
 		opcode_operand = get_opcode_operand(opcode, delim);
-		if (is_valid_opcode(line_number, opcode) != 0)
+		if (is_valid_opcode(line_number, opcode) != 0 && opcode != NULL)
 			return (EXIT_FAILURE);
 		if (strcmp(opcode, "push") == 0)
 		{
